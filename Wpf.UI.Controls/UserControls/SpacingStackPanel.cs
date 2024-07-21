d7105158bf91e185946a4751489a917f68962077
Вячеслав Жалковский <vzhalkovki@kitt.systems>
2024-07-21 22:39:36 +0300

    feat: implement icon control and buttons

---
+++ Wpf.UI.Controls/UserControls/SpacingStackPanel.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace Wpf.UI.Controls
+{
+    public class SpacingStackPanel : StackPanel
+    {
+        public int ItemSpacing
+        {
+            get { return (int)GetValue(ItemSpacingProperty); }
+            set { SetValue(ItemSpacingProperty, value); }
+        }
+        public static readonly DependencyProperty ItemSpacingProperty =
+            DependencyProperty.Register("ItemSpacing", typeof(int), typeof(SpacingStackPanel), new PropertyMetadata(0));
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+
+            switch (e.Property.Name)
+            {
+                case nameof(ItemSpacing):
+                    SetMarginChilds();
+                    break;
+            }
+        }
+
+        protected override void OnVisualChildrenChanged(DependencyObject visualAdded, DependencyObject visualRemoved)
+        {
+            base.OnVisualChildrenChanged(visualAdded, visualRemoved);
+
+            SetMarginChilds();
+        }
+
+        private void SetMarginChilds()
+        {
+            if (Children.Count == 0)
+                return;
+
+            for (int i = 0; i < Children.Count - 1; i++)
+            {
+                SetMargin(Children[i] as FrameworkElement);
+            }
+        }
+
+        private void SetMargin(FrameworkElement visual)
+        {
+            if (visual is null) return;
+
+            if (Orientation == Orientation.Horizontal)
+                visual.Margin = new Thickness(0, 0, ItemSpacing, 0);
+            else
+                visual.Margin = new Thickness(0, 0, 0, ItemSpacing);
+        }
+    }
+}
---
+++ Wpf.UI.Controls/UserControls/Switch.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Wpf.UI.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для Switch.xaml
+    /// </summary>
+    public partial class Switch : CheckBox
+    {
+
+        public string OffText
+        {
+            get { return (string)GetValue(OffTextProperty); }
+            set { SetValue(OffTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty OffTextProperty =
+            DependencyProperty.Register("OffText", typeof(string), typeof(Switch), new PropertyMetadata(string.Empty));
+
+
+        public string OnText
+        {
+            get { return (string)GetValue(OnTextProperty); }
+            set { SetValue(OnTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty OnTextProperty =
+            DependencyProperty.Register("OnText", typeof(string), typeof(Switch), new PropertyMetadata(string.Empty));
+
+
+
+        public Brush Fill
+        {
+            get { return (Brush)GetValue(FillProperty); }
+            set { SetValue(FillProperty, value); }
+        }
+
+        public static readonly DependencyProperty FillProperty =
+            DependencyProperty.Register("Fill", typeof(Brush), typeof(Switch), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(255, 255, 255))));
+
+
+    }
+}
